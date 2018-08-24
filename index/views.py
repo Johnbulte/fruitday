76b416e2cbaa4efc62ad6848effc7f753dd68025
Johnbulte <a760862802@outlook.com>
2018-08-24 17:33:22 +0800

    Initial commit

---
+++ index/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+def index_views(request):
+	return render(request, 'index.html')
+
+
+def login_views(request):
+	return render(request, 'login.html')
+
+
+def register_views(request):
+	return render(request, 'register.html')
+
+
+def templates_views(request):
+	return render(request,'login.html')
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!K:\py练习题\Django\fruitday\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
