30a80c1af3277777324a727ad515e2a62743e963
Johnbulte <a760862802@outlook.com>
2018-09-05 15:16:14 +0800

    2018-9-5更新-更新注册和登录

--- index/models.py
+++ index/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+	uphone = models.CharField(max_length=20)
+	upwd = models.CharField(max_length=20)
+	uemail = models.EmailField()
+	uname = models.CharField(max_length=20)
+
+	def __str__(self):
+		return self.uname
+
